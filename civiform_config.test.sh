bab3b84f5fb77c957dc6ebf2913a9e3c842ae092
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2026-02-05 14:09:56 -0800

    Update civiform_config.test.sh
    Updating version to v3.22

--- civiform_config.test.sh
+++ civiform_config.test.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="prod"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="v3.21.0"
+export CIVIFORM_VERSION="v3.22.0"
 
 # REQUIRED
 # Version of the infrastructure to use.
