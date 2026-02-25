9777bebfb62c2d85631a24dfcd00f8206d91faf3
Swathi Murthi <101214724+swatkat1@users.noreply.github.com>
2026-02-25 10:45:52 -0800

    Update CiviForm version and cloud deployment to patch version

--- civiform_config.test.sh
+++ civiform_config.test.sh
@@ -23,7 +23,7 @@ export CIVIFORM_MODE="prod"
 #
 # For dev and staging civiform modes, can be "latest". For prod, must be a version from
 # https://github.com/civiform/civiform/releases, for example "v1.2.3".
-export CIVIFORM_VERSION="v3.22.0"
+export CIVIFORM_VERSION="DEV-93321fb-v3.23.0-with-patch"
 
 # REQUIRED
 # Version of the infrastructure to use.
@@ -34,8 +34,8 @@ export CIVIFORM_VERSION="v3.22.0"
 #    depending on USE_DOCKER flag.
 #
 # Using "latest" is recommended.
-#export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="latest"
-export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="${CIVIFORM_VERSION}"
+export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="latest"
+#export CIVIFORM_CLOUD_DEPLOYMENT_VERSION="${CIVIFORM_VERSION}"
 
 # Terraform configuration
 #################################################
